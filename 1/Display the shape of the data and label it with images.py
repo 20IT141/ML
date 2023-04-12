dbf5d438bb9ace9c015b349249a22222f25f2ad2
20IT141 <89062234+20IT141@users.noreply.github.com>
2023-04-12 12:10:55 +0530

    Display the shape of the data and label it with images

---
+++ 1/Display the shape of the data and label it with images.py
@@ -0,0 +1,7 @@
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+train_images.shape
+len(train_labels)
+train_labels
+test_images.shape
+len(test_labels)
