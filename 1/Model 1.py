876ff15ea5ea05312813a15557ffd6b6fd74717e
20IT141 <89062234+20IT141@users.noreply.github.com>
2023-04-12 12:12:40 +0530

    Model 1.py

---
+++ 1/Model 1.py
@@ -0,0 +1,11 @@
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(28, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
